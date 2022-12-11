edd738346a52cb9c93997f80d46d1efd498e4caf
Banana Man <banana@bananaboogie.com>
2022-12-11 14:09:38 -0800

    Update counter style

--- src/components/Cookie/Cookie.tsx
+++ src/components/Cookie/Cookie.tsx
@@ -1,11 +1,13 @@
 //@ts-nocheck
 import { useState } from 'react';
+import Image from 'next/image';
 import styled from 'styled-components';
 
-import { COOKIE_PRICE } from '@constants/constants';
+import UnstyledButton from '@components/UnstyledButton';
 import PaymentForm from '@components/PaymentForm';
 import PaymentProvider from '@components/PaymentProvider';
 
+import { COOKIE_PRICE } from '@constants/constants';
 export default function Cookie() {
   const [cookieCounter, setCookieCounter] = useState(1);
   const [showCheckout, setShowCheckout] = useState(false);
@@ -22,26 +24,35 @@ export default function Cookie() {
             <PaymentForm />
           </div>
         ) : (
-          <div className="cookie-wrapper">
-            <button
-              className="subtract-cookie"
-              onClick={() =>
-                cookieCounter > 1 && setCookieCounter(cookieCounter - 1)
-              }
-            >
-              -
-            </button>
-            <span className="cookie-counter">{cookieCounter}</span>
-            <button
-              className="add-cookie"
-              onClick={() => setCookieCounter(cookieCounter + 1)}
-            >
-              +
-            </button>
-            <div>
-              <button onClick={() => setShowCheckout(true)}> BUY ME!</button>
-            </div>
-          </div>
+          <>
+            <CookieCounter>
+              <CounterButton
+                onClick={() =>
+                  cookieCounter > 1 && setCookieCounter(cookieCounter - 1)
+                }
+              >
+                <Image
+                  src="/minus-sign.svg"
+                  alt="minus-sign"
+                  width={72}
+                  height={72}
+                />
+              </CounterButton>
+              <CookieNumber>{cookieCounter}</CookieNumber>
+              <CounterButton
+                className="add-cookie"
+                onClick={() => setCookieCounter(cookieCounter + 1)}
+              >
+                <Image
+                  src="/plus-sign.svg"
+                  alt="plus-sign"
+                  width={72}
+                  height={72}
+                />
+              </CounterButton>
+            </CookieCounter>
+            <button onClick={() => setShowCheckout(true)}> BUY ME!</button>
+          </>
         )}
       </Wrapper>
     </PaymentProvider>
@@ -50,4 +61,25 @@ export default function Cookie() {
 
 const Wrapper = styled.main`
   flex: 1;
+  display: grid;
+  grid-template-rows: 2fr 100px;
+`;
+
+const CookieCounter = styled.div`
+  display: grid;
+  place-content: center;
+  grid-template-columns: minmax(50px, 1fr) 3fr minmax(50px, 1fr);
+  justify-items: center;
+`;
+
+const CounterButton = styled(UnstyledButton)`
+  display: grid;
+  place-content: center;
+  height: 100%;
+  width: 100%;
+`;
+
+const CookieNumber = styled.h1`
+  font-size: 222px;
+  text-align: center;
 `;
